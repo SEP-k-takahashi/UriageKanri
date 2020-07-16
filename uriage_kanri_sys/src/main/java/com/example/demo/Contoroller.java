8c496055c0abdcf69bf44c31996cb9d1cd9717c8
Kenta <Kenta@192.168.2.108>
2020-07-16 11:34:14 +0900

    UPD

--- uriage_kanri_sys/src/main/java/com/example/demo/Contoroller.java
+++ uriage_kanri_sys/src/main/java/com/example/demo/Contoroller.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -28,5 +29,18 @@ public String getAllUsers(@Validated AnkenEntity Anken,  BindingResult result,
 	model.addAttribute("anken", page);
 
 return "list";
+}
+/**
+ * 案件新規登録画面を表示
+ * @param model Model
+ * @return 案件新規登録画面
+ */
+@GetMapping(value = "/add")
+public String displayAdd(Model model) {
+
+
+  model.addAttribute("ankenRequest", new AnkenRequest());
+  //model.addAttribute("category",category);
+  return "add";
 }
 }
